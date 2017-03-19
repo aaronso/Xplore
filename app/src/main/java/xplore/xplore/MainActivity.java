2c3e0cb4048f94dcc6b95d8763d008c8f11f812c
JessicaNgo <jessica.ngo@live.com>
2017-03-18 22:20:54 -0700

    yay button does something

--- app/src/main/java/xplore/xplore/MainActivity.java
+++ app/src/main/java/xplore/xplore/MainActivity.java
@@ -1,19 +1,38 @@
 package xplore.xplore;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button splashButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, Xplore.class);
-        startActivity(i);
+        loadCustomFont();
+
+        splashButton.setOnClickListener(this);
+
+    }
+
+    private void loadCustomFont(){
+        splashButton = (Button)findViewById(R.id.splash_button);
+        Typeface FontTypo = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Bold.ttf");
+        splashButton.setTypeface(FontTypo);
 
     }
 
 
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, Xplore.class);
+        startActivity(i);
+    }
 }
